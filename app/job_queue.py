b0a55faa3716274fde2f7dc04b5544bdd74fb4f4
Hassan Teymoori <h.teymoori@tue.nl>
2024-07-22 12:46:45 +0200

    retry true in celery

--- app/job_queue.py
+++ app/job_queue.py
@@ -2,13 +2,14 @@ from celery import Celery
 
 
 def make_celery(app):
-    celery_obj = Celery(app.import_name)
-    celery_obj.conf.update(app.config['CELERY_CONFIG'])
+    celery_app = Celery(app.import_name)
+    celery_app.conf.update(app.config['CELERY_CONFIG'])
+    celery_app.conf.broker_connection_retry_on_startup = True
 
-    class ContextTask(celery_obj.Task):
+    class ContextTask(celery_app.Task):
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return self.run(*args, **kwargs)
 
-    celery_obj.Task = ContextTask
-    return celery_obj
+    celery_app.Task = ContextTask
+    return celery_app
