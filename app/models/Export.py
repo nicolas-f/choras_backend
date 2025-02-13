90ba283434cc7414ed2726a0270800f25199315a
Timo <631044206@qq.com>
2025-02-13 11:53:40 +0100

    fix the backref for simulation
    exports -> export
    
    patch for 8404c75

--- app/models/Export.py
+++ app/models/Export.py
@@ -10,7 +10,7 @@ class Export(db.Model):
     preCsvFileName = db.Column(db.String(), nullable=False)
     
     simulationId = db.Column(db.Integer, db.ForeignKey("simulations.id", ondelete="CASCADE"), nullable=False)
-    simulation = db.relationship("Simulation", backref=db.backref("exports", uselist=False, cascade="all, delete"), foreign_keys=[simulationId])
+    simulation = db.relationship("Simulation", backref=db.backref("export", uselist=False, cascade="all, delete"), foreign_keys=[simulationId])
     
     createdAt = db.Column(db.String(), default=datetime.now())
     updatedAt = db.Column(db.String(), default=datetime.now())
