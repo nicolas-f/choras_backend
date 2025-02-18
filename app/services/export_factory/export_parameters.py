64e15676959b160a9f095bf8e4a9b064e92005d7
bbaigalmaa <baigal815@gmail.com>
2025-02-18 16:10:08 +0100

    Execute_export updated.

--- app/services/export_factory/export_parameters.py
+++ app/services/export_factory/export_parameters.py
@@ -1,10 +1,10 @@
-class ExportParameters(ExportStrategy):
-    def export(self, params):
-        params should contain simulation id
-        fecth excel file name from simulation id
+# class ExportParameters(ExportStrategy):
+#     def export(self, params):
+#         params should contain simulation id
+#         fecth excel file name from simulation id
 
-        extract parameters from the excel
-        - check on parameters tab
-        - extract requested data into csv use helper tools
-        - temporarily store the data
-        return "array of path to simulation_results.csv"
+#         extract parameters from the excel
+#         - check on parameters tab
+#         - extract requested data into csv use helper tools
+#         - temporarily store the data
+#         return "array of path to simulation_results.csv"
--- app/services/export_factory/factory.py
+++ app/services/export_factory/factory.py
@@ -1,17 +1,17 @@
 from app.services.export_factory.export_parameters import ExportParameters
 
 
-class ExportFactory:
-    strategies = {
-        "parameters": ExportParameters(),
-        "plot": PlotExport(),
-        "new_results": NewResultsExport(),
-    }
+# class ExportFactory:
+#     strategies = {
+#         "parameters": ExportParameters(),
+#         "plot": PlotExport(),
+#         "new_results": NewResultsExport(),
+#     }
 
-    @staticmethod
-    def get_exporter(resource_type):
-        return ExportFactory.strategies.get(resource_type, None)
+#     @staticmethod
+#     def get_exporter(resource_type):
+#         return ExportFactory.strategies.get(resource_type, None)
 
-    def get_export_types -> array of export_type
+#     def get_export_types -> array of export_type
 
 
