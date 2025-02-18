8e4ee8d85b8f3f7f485d85e20307d9bcb58db358
Timo <631044206@qq.com>
2025-02-18 15:49:30 +0100

    export redesign
    example:
    
    ```python3
    from app.services.export_service import ExportHelper
    from config import DefaultConfig
    import os
    
    if __name__ == '__main__':
        xlsx_path = os.path.join(DefaultConfig.UPLOAD_FOLDER_NAME, "MeasurementRoom_8ed927b9dd0c40be8da520e95f8d4a23_5.xlsx")
    
        exportHelper = ExportHelper()
    
        zip_binary = exportHelper.extract_from_xlsx_to_csv_to_zip_binary(xlsx_path, {"Parameters": ["edt", "ts"], "EDC": ["t", "125Hz"]})
    
        exportHelper.write_file_to_zip_binary(zip_binary, "README.md")
    
        with open("test.zip", "wb") as f:
            f.write(zip_binary.getvalue())
    
        print("Done")
    ```

--- app/routes/export.py
+++ app/routes/export.py
@@ -1,15 +1,16 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint
-from flask import send_from_directory
+from flask import send_from_directory, send_file
 
 from app.services import export_service
 
 blp = Blueprint("Export", __name__, description="Export API")
 
 
+# TODO: @almasmuhtadi @bbaigalmaa
 @blp.route("/exports/<int:simulation_id>")
 class ExportList(MethodView):
     @blp.response(200, content_type="application/zip")
     def get(self, simulation_id):
-        zip_path = export_service.get_zip_path_by_sim_id(simulation_id)
-        return send_from_directory(zip_path.parent, zip_path.name, as_attachment=True)
+        zip_binary = export_service.get_zip_path_by_sim_id(simulation_id)
+        return send_file(zip_binary, as_attachment=True)
