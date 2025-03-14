db1d6796e3667273d233dbbbd7ef41e77b0871df
almas <almas.hilman@gmail.com>
2025-03-14 10:26:23 +0100

    add usage guidance

--- config.py
+++ config.py
@@ -157,6 +157,7 @@ class CustomExportParametersConfig(DefaultConfig):
 
 
 class FeatureToggle(DefaultConfig):
+    # Uncomment this line to enable geo conversion from input geometry
     # enable_geo_conversion = True
 
     @classmethod
