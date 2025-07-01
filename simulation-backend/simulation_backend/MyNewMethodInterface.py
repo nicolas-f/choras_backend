fcfb2dad3ed325558adbe5b1563f770ccccc86e7
SilvinWillemsen <silvinwillemsen@gmail.com>
2025-07-01 15:00:33 +0200

    Fixed issue with importing from MyNewMethod

--- simulation-backend/simulation_backend/MyNewMethodInterface.py
+++ simulation-backend/simulation_backend/MyNewMethodInterface.py
@@ -1,7 +1,7 @@
 # This is the example interface file for connecting CHORAS to your method
 
 # Import the relevant functions from 
-from MyNewMethod.My_New_Method import simulation_method
+from My_New_Method import simulation_method
 
 # This function will be called from app/services/simulation_service.py
 def mynewmethod_method (json_file_path=None):
