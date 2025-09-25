13516f950cb5da12bceba3ec92e9d86ebe93b75e
nicolas-f <1382241+nicolas-f@users.noreply.github.com>
2025-09-25 17:03:32 +0200

    configure pyproject to build every isimpa project except user interface

--- spps/setup.py
+++ spps/setup.py
@@ -4,5 +4,7 @@ setup(
     name="spps",
     version="1.0.0",
     description="Spps computation code",
+    cmake_args=['-DSKIPISIMPA:BOOL=ON'],
+    cmake_source_dir = "I-Simpa",
     packages=['spps'],
 )
